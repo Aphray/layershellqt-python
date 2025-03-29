431f369da8c750bf1e16e02608f32f4ebfdfce84
Bryce Kingsley <brycejkingsley@gmail.com>
2025-03-29 17:09:54 -0400

    update example

--- examples/main.py
+++ examples/main.py
@@ -1,47 +1,43 @@
 import sys
-from PySide6.QtCore import QMargins, Qt
-from PySide6.QtWidgets import QWidget, QFrame, QPushButton, QHBoxLayout, QApplication, QStyleFactory
 
-
+from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout, QApplication, QFrame
+from PySide6.QtCore import QMargins
 from QtLayerShell import QLayerShell
 
-            
+
+
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
-
-    screens = app.screens()
-    
-    btn = QPushButton('test')
-    
-    
-    ls = QLayerShell(
-        scope='test-shell',
-        screen=screens[1],
-        layer=QLayerShell.Layer.Top,
-        margins=QMargins(0, 0, 500, 0),
-        exclusiveZone=30,
-        exclusiveEdge=QLayerShell.Anchor.Left
-    )
     
     widget = QFrame()
-    ls.setWidget(widget)
+    button = QPushButton()
+    button.clicked.connect(lambda: print('Hello!'))
     
-    l = QHBoxLayout()
-    l.addWidget(btn)
-    widget.setLayout(l)
+    layout = QHBoxLayout()
+    layout.addWidget(button)
     
-    widget.move(400, 200)
-    widget.setFixedWidth(500)
-    widget.setFixedHeight(500)
+    # widget.setLayout(layout)
+    
+    widget.setObjectName('Widget')
     widget.setStyleSheet(
-        '''QFrame {
-            background-color: rgba(30, 30, 30, 0.9);
+        '''
+        QFrame#Widget {
+            background: rgba(14, 14, 14, 0.05);
             border-radius: 10px;
-        }
+        }             
         ''')
     
-    ls.show()
     
+    layer = QLayerShell(
+        widget,
+        'test-shell',
+        margins=QMargins(10, 10, 10, 1050),
+        exclusiveZone=30,
+        exclusiveEdge=QLayerShell.Anchor.Top
+    )
+    
+    layer.show()
+    
+    print('running')
     sys.exit(app.exec())
-    
